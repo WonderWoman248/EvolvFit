8ba0bc899ccd926a6200faa0bd15338a4936a8da
prashansa15 <prashansa15111784@gmail.com>
2021-05-21 21:24:19 +0530

    Updates

--- src/Reducers/index.js
+++
@@ -1,7 +0,0 @@
-import login from './login';
-import { combineReducers } from 'redux'
-import adminData from './adminData'
-
-const rootReducer = combineReducers({ login,adminData})
-
-export default rootReducer
